c52f7b713388c4992d809bee0be8e9ffffb99e38
Anatoliy Sysoienko <pistalierro@gmail.com>
2025-05-29 15:35:38 +0300

    init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MainLayoutComponent} from '../layouts/main-layout/main-layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('../pages/catalog/catalog.component')
+          .then(m => m.CatalogComponent)
+      },
+      {
+        path: 'product/:id',
+        loadComponent: () => import('../pages/product/product.component')
+          .then(m => m.ProductComponent)
+      },
+      {
+        path: 'cart',
+        loadComponent: () => import('../pages/cart/cart.component')
+          .then(m => m.CartComponent)
+      },
+      {
+        path: 'checkout',
+        loadComponent: () => import('../pages/checkout/checkout.component')
+          .then(m => m.CheckoutComponent)
+      }
+    ]
+  },
+  {path: '**', redirectTo: ''}
+];
---
+++ src/layouts/main-layout/header/catalog-trigger/catalog-trigger.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalog-trigger',
+  imports: [],
+  templateUrl: './catalog-trigger.component.html',
+  styleUrl: './catalog-trigger.component.scss'
+})
+export class CatalogTriggerComponent {
+
+}
